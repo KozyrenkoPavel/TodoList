0362ed1430b23e0d6d0b1e2529889954f4ecca1e
Pavel Kozyrenko <kozyrenko_pavlik@mail.ru>
2023-01-22 11:22:34 +0300

    Finished project TodoList

---
+++ src/components/Todos/TodoList.js
@@ -0,0 +1,23 @@
+import Todo from './Todo';
+import styles from './TodoList.module.css';
+
+function TodoList({ todos, deleteTodo, toggleTodo }) {
+  return (
+    <div className={styles.todoListContainer}>
+      {todos.length ? (
+        todos.map((todo) => (
+          <Todo
+            key={todo.id}
+            todo={todo}
+            deleteTodo={deleteTodo}
+            toggleTodo={toggleTodo}
+          />
+        ))
+      ) : (
+        <h2>Todo list is empty</h2>
+      )}
+    </div>
+  );
+}
+
+export default TodoList;
